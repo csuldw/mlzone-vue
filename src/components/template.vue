a2a6727eba64c780444c9134138eec323450e7f2
Diwei Liu <csu.ldw@csu.edu.cn>
2017-11-03 17:54:43 +0800

    Create template.vue

---
+++ src/components/template.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="g-container">
+     <header-select></header-select>
+		<div class="g-container-down">
+      <div class="g-container-wrap">
+        <div class="g-content">
+          <div class="">
+
+          </div>
+        </div>
+      </div>
+      <div class="g-sidebar">
+        <div class=" ">
+
+        </div>
+      </div>
+		</div>
+	</div>
+</template>
+
+<script>
+import '../assets/css/grid.css'; //引入外部css
+import header from './base/header.vue';
+import footer from './base/footer.vue';
+
+export default {
+  components: {
+    'header-select': header,
+    'footer-select': footer
+  },
+	data () {
+		return {
+		}
+	},
+	computed: {
+	},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
