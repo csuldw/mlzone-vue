2188454828b3e9e2ad5df0717d8c073403307e8c
Diwei Liu <csu.ldw@csu.edu.cn>
2017-11-21 15:15:12 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import Page from '@/components/Page.vue'
 import Post from '@/components/Post.vue'
 import BugManagement from './components/bugManagement/BugManagement.vue'
 import UserDetail from '@/components/UserDetail'
+import Links from '@/components/Links'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
 
 const routes = [
   {
@@ -39,6 +42,21 @@ const routes = [
     path: '/userDetail',
     name: 'UserDetail',
     component: UserDetail //resolve => require(['components/UserDetail.vue'], resolve).default
+  },
+  {
+    path: '/links',
+    name: 'links',
+    component: Links
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
   }
 ]
 export default routes
