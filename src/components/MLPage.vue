2fd33e4a1cae6b6fc682d45d1e7df5a785ac3312
Diwei Liu <csu.ldw@csu.edu.cn>
2017-12-28 21:22:54 +0800

    Update MLPage.vue

--- src/components/MLPage.vue
+++ src/components/MLPage.vue
@@ -44,7 +44,6 @@
               <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
-                :current-page="currentPage4"
                 :page-sizes="[5, 10, 20, 30, 40]"
                 :page-size="queryData.pageSize"
                 layout="total, sizes, prev, pager, next, jumper"
