66a4babdd638a60d26c6bdd14e2352abce6e812f
Diwei Liu <csu.ldw@csu.edu.cn>
2018-01-02 18:10:41 +0800

    Update AboutUs.vue

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -18,16 +18,15 @@
       </div>
       <div class="g-container-content">
         <div class="containter-right">
-          <header class="containter-right-header">
-            <span class="container-title">关于MLZero</span>
-          </header>
-          <article class="article-content">
-            <p>MLZone是一个专门收集机器学习相关资料的网站。</p>
-            <p>&nbsp;</p>
-            <h2>问：MLZone会涉及哪些领域呢？</h2>
+          <div class="article-content">
+            <p>MLZero是一个专门发布与机器学习、后端相关资料的网站。</p><p></p>
+            <h2>Q1：MLZero会涉及哪些领域呢？</h2>
             <p>答：主要是机器学习，其次还有大数据、数据挖掘、人工智能、云计算等领域的相关内容。</p>
-          </article>
-          <p>&nbsp;</p>
+          </div>
+          <div class="u-container-content">
+
+
+          </div>
         </div>
       </div>
       <!--<div class="g-post-plugin-comment">
