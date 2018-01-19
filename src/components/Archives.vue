6f7f04a14173b872c82b4b911bbe0cf2a3ed7659
Diwei Liu <csu.ldw@csu.edu.cn>
2018-01-19 17:33:59 +0800

    Update Archives.vue

--- src/components/Archives.vue
+++ src/components/Archives.vue
@@ -16,12 +16,16 @@
             <h1>文章列表</h1>
           </div>
           <div class="u-archive-content" >
-            <article class="u-archive-main" v-for="article in articleList">
-              <router-link :to="{ name: 'PostDetail', params: { articleId: article.id, path: getFileName(article.filePath) }}" target="_blank">
-                {{getDateFromStr(article.publicDate)}}
-                [{{article.articleCategoryEntity.categoryName}}] {{ article.title }}
-              </router-link>
-            </article>
+            <div class="u-archive-body">
+              <article class="u-archive-main" v-for="article in articleList">
+                <router-link :to="{ name: 'PostDetail', params: { articleId: article.id, path: getFileName(article.filePath) }}" target="_blank">
+                  <header class="post-header">
+                    <div class="post-meta">{{getDateFromStr(article.publicDate)}}</div>
+                    <span class="post-title">[{{article.articleCategoryEntity.categoryName}}] {{ article.title }}</span>
+                  </header>
+                </router-link>
+              </article>
+            </div>
           </div>
           <div class="archive-pagination-meta">
             <el-pagination
@@ -44,19 +48,64 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .u-archive-body{
+    position: relative;
+    border-left: 1px solid #ececec;
+  }
   .u-archive-main{
-    padding: 22px 10px 0px 10px;
+    padding: 20px 10px 15px 0;
+    transition-duration: 0.2s;
+    transition-timing-function: ease-in-out;
+    transition-delay: 0s;
+    transition-property: border;
+    position: relative;
+  }
+  .archive-pagination-meta{
+    padding: 0 20px 50px 0;
+    margin-top: 50px;
+  }
+  .u-archive-main::before {
+    content: " ";
+    left: 0;
+    top: 26px;
+    position: absolute;
+    width: 6px;
+    height: 6px;
+    margin-left: -4px;
+    background: #bbb;
+    border-radius: 50%;
+    border: 1px solid #fff;
+    transition-duration: 0.2s;
+    transition-timing-function: ease-in-out;
+    transition-delay: 0s;
+    transition-property: background;
+  }
+  .u-archive-main:hover::before{
+    background: #222;
+  }
+  .post-header {
+    padding-left: 10px;
+    position: relative;
     transition-duration: 0.2s;
     transition-timing-function: ease-in-out;
     transition-delay: 0s;
     transition-property: border;
     border-bottom: 1px dashed #ccc;
   }
-  .u-archive-main:hover {
+  .post-header:hover {
     border-bottom-color: #666;
   }
-  .archive-pagination-meta{
-    padding: 0 20px 50px 0;
-    margin-top: 50px;
+  .post-header .post-meta {
+    position: absolute;
+    font-size: 12px;
+    left: 20px;
+    top: 5px;
+  }
+  .post-header .post-title {
+    margin-left: 120px;
+    font-size: 16px;
+    font-weight: normal;
+    font-family: Lato, "PingFang SC", "Microsoft YaHei", sans-serif;
+    line-height: inherit;
   }
 </style>
